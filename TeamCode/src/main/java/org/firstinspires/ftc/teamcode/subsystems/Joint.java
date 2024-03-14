2d9aa71a2e26ea5927ff68a0decbb24cef30df7c
Stefan <sad@stephn.codes>
2024-03-14 23:45:50 +0200

    positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Joint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Joint.java
@@ -7,8 +7,8 @@ import com.qualcomm.robotcore.hardware.ServoImplEx;
 public class Joint {
     ServoImplEx joint;
 
-    public static double JOINT_COLLECT = 0; // TODO: find positions
-    public static double JOINT_SCORE = 1;
+    public static double JOINT_COLLECT = 1; // TODO: find positions
+    public static double JOINT_SCORE = 0.65;
 
     public Joint(HardwareMap hardwareMap) {
         joint = hardwareMap.get(ServoImplEx.class, "joint");
@@ -23,6 +23,6 @@ public class Joint {
     }
 
     public void setPosition(double position) {
-        joint.setPosition(0);
+        joint.setPosition(position);
     }
 }
